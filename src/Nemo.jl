c487aab6d19cb679edb30d0545cefeabf8eed23d
Tommy Hofmann <thofma@gmail.com>
2022-10-08 17:55:33 +0200

    Update banner logic (#1346)

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -223,9 +223,20 @@ function __init__()
    ccall((:flint_set_abort, libflint), Nothing,
          (Ptr{Nothing},), @cfunction(flint_abort, Nothing, ()))
 
-   # Check if were are non-interactive
+   # Check if were loaded from another package
+   # if VERSION < 1.7.*, only the "other" package will have the
+   # _tryrequire_from_serialized in the backtrace.
+   # if VERSION >= 1.8, also doing 'using Package' will have
+   # _tryrequire_from_serialized the backtrace.
+   #
+   # To still distinguish both scenarios, notice that
+   # 'using OtherPackage' will either have _tryrequire_from_serialized at least twice,
+   # or one with four arguments (hence five as the function name is the first argument)
+   # 'using Package' serialized will have a version with less arguments
    bt = Base.process_backtrace(Base.backtrace())
-   isinteractive_manual = all(sf -> sf[1].func != :_tryrequire_from_serialized, bt)
+   filter!(sf -> sf[1].func === :_tryrequire_from_serialized, bt)
+   isinteractive_manual =
+      length(bt) == 0 || (length(bt) == 1 && length(only(bt)[1].linfo.specTypes.parameters) < 4)
 
    # Respect the -q and --banner flag
    allowbanner = Base.JLOptions().banner != 0
