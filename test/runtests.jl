a45165bb0b3ffa8fe1a85606ab0e40694f837835
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-31 00:16:26 +0200

    Add `Aqua.jl` for some static testing (#1485)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ using InteractiveUtils: @which
 import Nemo.AbstractAlgebra
 include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
 
+include("Aqua.jl")
 include("rand.jl")
 include("Nemo-test.jl")
