045e72d9a299cbb33a2546903e671a3090a5468f
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-04-29 11:11:51 +0200

    Adapt Aliases.jl for breaking release (#1736)

--- src/Aliases.jl
+++ src/Aliases.jl
@@ -2,24 +2,12 @@
 # (this list contained stuff along the lines of `@alias is_equal isequal`, but everything has moved to AbstractAlgebra)
 
 
-# TODO: next breaking release: remove the if guard around the block
-if @__MODULE__() == Nemo
+# alternative names for some functions from LinearAlgebra
+# we don't use the `@alias` macro here because we provide custom
+# docstrings for these aliases
+const eigenvalues = eigvals
 
-    # alternative names for some functions from LinearAlgebra
-    # we don't use the `@alias` macro here because we provide custom
-    # docstrings for these aliases
-    const eigenvalues = eigvals
-end
 
 # predeclare some functions to allow defining aliases for some of our own functions
 function eigenvalues_simple end
 @alias eigvals_simple eigenvalues_simple # for consistency with eigvals/eigenvalues
-
-
-# TODO: next breaking release: remove this block
-# Oscar includes this file for historical reasons, but expects it to contain the Base.@deprecate_binding calls.
-# Until this is changed and released there, we thus need to include the deprecations here.
-@static if Symbol(@__MODULE__()) in [:Hecke, :Oscar]
-    Nemo.@include_deprecated_bindings()
-end
-
