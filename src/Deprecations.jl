b04719f3fe9f403b98cfd675f48c3215ee77665e
Tommy Hofmann <thofma@gmail.com>
2024-05-17 23:32:43 +0200

    bug: undeprecate and bump to 0.45.3 (#1756) (#1757)
    bug: undepracte and bump to 0.45.3 (#1756)

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -64,10 +64,10 @@ end
 
 # Deprecated in 0.45.*
 @deprecate defining_polynomial(Q::fqPolyRepField, P::Ring) defining_polynomial(P, Q)
-@deprecate lift(a::PadicFieldElem) lift(ZZ, a)
-@deprecate prime_field(k::PadicField) base_field(k)
+lift(a::PadicFieldElem) = lift(ZZ, a)
+prime_field(k::PadicField) = base_field(k)
 
 function (R::QadicField)(n::ZZPolyRingElem, pr::Int)
-  Base.depwarn("`(::QadicField)(::ZZPolyRingElem, ::Int)` is deprecated, use `(::QadicField)(::ZZPolyRingElem; precision::Int)` instead.", :QadicField)
+  #Base.depwarn("`(::QadicField)(::ZZPolyRingElem, ::Int)` is deprecated, use `(::QadicField)(::ZZPolyRingElem; precision::Int)` instead.", :QadicField)
   return (R::QadicField)(n::ZZPolyRingElem; precision=pr)
 end
