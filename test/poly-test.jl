1b3e5ddb8133938e1710e5135804c16b820b3450
Johannes Schmitt <jschmitt@posteo.eu>
2024-10-08 12:42:12 +0200

    Enable a test (#1882)

--- test/poly-test.jl
+++ test/poly-test.jl
@@ -4,8 +4,7 @@
   f = (x - 1)^2 * x^2 * (x + 1)
   sf = factor_squarefree(f)
   @test length(sf) == 2
-  # This is broken in AbstractAlgebra https://github.com/Nemocas/AbstractAlgebra.jl/pull/1799
-  #@test all(is_squarefree, keys(sf.fac))
+  @test all(is_squarefree, keys(sf.fac))
   @test evaluate(sf) == f
 
   sf = factor_squarefree(Kx(1))
