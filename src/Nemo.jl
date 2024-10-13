a9fc5163bb87961fd433ecc9192ec3b67933f904
Alexander Demin <asdemin_2@edu.hse.ru>
2024-10-13 15:02:32 +0300

    Test show_banner

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -329,6 +329,13 @@ end
 #
 ################################################################################
 
+function show_banner()
+  println("")
+  println("Welcome to Nemo version $(version())")
+  println("")
+  println("Nemo comes with absolutely no warranty whatsoever")
+end
+
 const __isthreaded = Ref(false)
 
 function __init__()
@@ -346,10 +353,7 @@ function __init__()
         (Ptr{Nothing},), @cfunction(flint_abort, Nothing, ()))
 
   if AbstractAlgebra.should_show_banner() && get(ENV, "NEMO_PRINT_BANNER", "true") != "false"
-    println("")
-    println("Welcome to Nemo version $(version())")
-    println("")
-    println("Nemo comes with absolutely no warranty whatsoever")
+    show_banner()
   end
 
   # Initialize the thread local random state
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using InteractiveUtils: @which
 import Nemo.AbstractAlgebra
 include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
 
+Nemo.show_banner()
 include("Aqua.jl")
 include("rand.jl")
 include("Nemo-test.jl")
